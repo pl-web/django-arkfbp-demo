7597bf7fd08584d72b40cf5103593df98fdd461e
it-reader <hnature@126.com>
2020-12-12 09:53:07 +0800

    demo

--- demo/book/getbook.py
+++ demo/book/getbook.py
@@ -1,5 +1,6 @@
 from . import models
-
+from django.http import HttpResponse
 def get_book(request):
   identity = request.GET.get('identity')
-  return models.Book.objects.filter(identity=identity)
+  return HttpResponse('get book identity %s', identity)
+  # return models.Book.objects.filter(identity=identity)
